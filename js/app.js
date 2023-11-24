4571363b764a4c2949159e90ca9a3d55a8b34187
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-11-24 18:09:00 -0300

    se agrego funcionalidad al maquetado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+const botonComenzarJuego = document.querySelector("#btnComenzar");
+const botonEnviar = document.querySelector("#btnEnviar");
+
+let numeroAleatorio = 0;
+
+function numeroAlAzar() {
+  numeroAleatorio = Math.floor(Math.random() * 10);
+  console.log(numeroAleatorio);
+}
+
+function enviarNumero() {
+    const numeroElegido = document.querySelector('input');
+    
+    if (numeroAleatorio == numeroElegido.value){
+        alert('¡FELICIDADES!, Los numeros son identicos');
+    }else if (numeroAleatorio > numeroElegido.value){
+        alert('El numero ingresado es menor');
+    }else {
+        alert('El numero ingresado es mayor');
+    }
+}
+
+botonComenzarJuego.addEventListener("click", numeroAlAzar);
+
+botonEnviar.addEventListener("click", enviarNumero);
 
