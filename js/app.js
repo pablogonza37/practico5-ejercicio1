0846fd551867e7a24c15e93d29fa25240360a172
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-02-05 23:47:49 -0300

    se corrigio el input y se agrego preventDefault para que no se actualice la pagina

--- js/app.js
+++ js/app.js
@@ -1,43 +1,60 @@
 const botonComenzarJuego = document.querySelector("#btnComenzar");
 const botonEnviar = document.querySelector("#btnEnviar");
 const numeroIntentos = document.querySelector("#intentos");
-const adivinar = document.querySelector('#adininar');
+const adivinar = document.querySelector("#adininar");
+const numeroElegido = document.querySelector("#numero");
 
 let numeroAleatorio = "";
 let intentos = 3;
 
 function numeroAlAzar() {
-  numeroAleatorio = Math.floor(Math.random() * 11); // decidi limitar los numeros del 1 al 10 para hacer mas facil la prueba
-  botonComenzarJuego.disabled = true;
-  adivinar.innerHTML = 'Adivinar el número';
+  numeroAleatorio = Math.floor(Math.random() * 10) + 1;
   console.log(numeroAleatorio);
+  botonComenzarJuego.disabled = true;
+  adivinar.innerHTML = "Adivinar el número";
+  numeroElegido.disabled = false;
+  numeroIntentos.innerHTML = "Numero de intentos: 3";
+  botonEnviar.disabled = false;
 }
 
-function enviarNumero() {
-  const numeroElegido = document.querySelector("input");
+function enviarNumero(event) {
+  event.preventDefault(); // Evita que el formulario se envíe y la página se actualice
+  
   if (numeroElegido.value < 0) {
     alert("Ingrese un numero positivo");
   } else {
     if (numeroAleatorio == numeroElegido.value) {
       alert("¡FELICIDADES!, has adivinado el numero magico");
-      location.reload();
+      // No es recomendable recargar la página con location.reload() para reiniciar el juego
+      reiniciarJuego();
     } else if (numeroAleatorio > numeroElegido.value) {
       alert("El numero ingresado es menor al numero magico");
       intentos -= 1;
       numeroIntentos.innerHTML = `Numero de intentos: ${intentos}`;
-
+      numeroElegido.value = "";
     } else {
       alert("El numero ingresado es mayor al numero magico");
       intentos -= 1;
       numeroIntentos.innerHTML = `Numero de intentos: ${intentos}`;
+      numeroElegido.value = "";
     }
 
-    if (intentos === 0){
-        alert('Game over, Has acabado tus intentos');
-        location.reload();
+    if (intentos === 0) {
+      alert("Game over, Has acabado tus intentos");
+      reiniciarJuego();
     }
   }
-
 }
 
+function reiniciarJuego() {
+  botonComenzarJuego.disabled = false;
+  adivinar.innerHTML = "¡Comenzar Juego!";
+  numeroElegido.disabled = true;
+  numeroIntentos.innerHTML = "";
+  botonEnviar.disabled = true;
+  location.reload();
+}
+
+
 botonComenzarJuego.addEventListener("click", numeroAlAzar);
+//botonEnviar.addEventListener("submit", enviarNumero);
